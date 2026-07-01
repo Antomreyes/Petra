59ff2acc2fd6fc13b7085ae5018c88a74d982374
Antomreyes <antonia.munozr@usm.cl>
2026-07-01 17:04:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 const productos = [
-  { id: 1, nombre: 'Cartera Roma', categoria: 'carteras', precio: '52.000', foto: './IMG_6118.JPG' },
-  { id: 2, nombre: 'Cartera Sicilia', categoria: 'carteras', precio: '62.000', foto: '' },
-  { id: 3, nombre: 'Cartera Bolonia', categoria: 'carteras', precio: '72.000', foto: '' },
-  { id: 4, nombre: 'Cartera Celular', categoria: 'carteras', precio: '12.000', foto: '' },
-  { id: 5, nombre: 'Neceser Mediano', categoria: 'neceser', precio: '25.000', foto: '' },
-  { id: 6, nombre: 'Neceser Grande', categoria: 'neceser', precio: '30.000', foto: '' },
-  { id: 7, nombre: 'Cosmetiquero', categoria: 'neceser', precio: '32.000', foto: '' },
-  { id: 8, nombre: 'Billetera', categoria: 'billeteras', precio: '23.000', foto: '' },
-  { id: 9, nombre: 'Billetera Hombre', categoria: 'billeteras', precio: '18.000', foto: '' },
-  { id: 10, nombre: 'Chequera', categoria: 'billeteras', precio: '26.000', foto: '' },
-  { id: 11, nombre: 'Tarjetero Mini', categoria: 'accesorios', precio: '9.000', foto: '' },
-  { id: 12, nombre: 'Porta Pasaporte', categoria: 'accesorios', precio: '12.000', foto: '' },
-  { id: 13, nombre: 'Agenda Chica', categoria: 'accesorios', precio: '15.000', foto: '' },
-  { id: 14, nombre: 'Agenda Grande', categoria: 'accesorios', precio: '20.000', foto: '' },
-  { id: 15, nombre: 'Porta PC', categoria: 'accesorios', precio: '', foto: '' },
-  { id: 16, nombre: 'Guante', categoria: 'accesorios', precio: '15.000', foto: '' },
+  { id: 1, nombre: 'Billetera Burdeo', categoria: 'billeteras', precio: '23.000', foto: './IMG_6118.JPG' },
+  { id: 2, nombre: 'Billetera Julia', categoria: 'billeteras', precio: '23.000', foto: '' },
+  { id: 3, nombre: 'Billetera Hombre', categoria: 'billeteras', precio: '18.000', foto: '' },
+  { id: 4, nombre: 'Chequera', categoria: 'billeteras', precio: '26.000', foto: '' },
+  { id: 5, nombre: 'Cartera Roma', categoria: 'carteras', precio: '52.000', foto: '' },
+  { id: 6, nombre: 'Cartera Sicilia', categoria: 'carteras', precio: '62.000', foto: '' },
+  { id: 7, nombre: 'Cartera Bolonia', categoria: 'carteras', precio: '72.000', foto: '' },
+  { id: 8, nombre: 'Cartera para Celular', categoria: 'carteras', precio: '12.000', foto: '' },
+  { id: 9, nombre: 'Neceser Grande', categoria: 'neceser', precio: '30.000', foto: '' },
+  { id: 10, nombre: 'Neceser Mediano', categoria: 'neceser', precio: '25.000', foto: '' },
+  { id: 11, nombre: 'Cosmetiquero Print', categoria: 'neceser', precio: '32.000', foto: '' },
+  { id: 12, nombre: 'Cosmetiquero Dorado', categoria: 'neceser', precio: '32.000', foto: '' },
+  { id: 13, nombre: 'Cosmetiquero Animal Print', categoria: 'neceser', precio: '32.000', foto: '' },
+  { id: 14, nombre: 'Tarjetero', categoria: 'accesorios', precio: '9.000', foto: '' },
+  { id: 15, nombre: 'Porta Pasaporte', categoria: 'accesorios', precio: '12.000', foto: '' },
+  { id: 16, nombre: 'Agenda Pequeña', categoria: 'accesorios', precio: '15.000', foto: '' },
+  { id: 17, nombre: 'Agenda Grande', categoria: 'accesorios', precio: '20.000', foto: '' },
+  { id: 18, nombre: 'Notebook Case', categoria: 'accesorios', precio: '40.000', foto: '' },
+  { id: 19, nombre: 'Guantes Parrilleros', categoria: 'accesorios', precio: '15.000', foto: '' },
 ];
 
 let categoriaActiva = 'todos';
@@ -39,7 +42,9 @@ function renderProductos() {
 
   grid.innerHTML = lista.map(p => `
     <div class="card" onclick="abrirProducto(${p.id})">
-      ${p.foto ? `<img class="card-img" src="${p.foto}" alt="${p.nombre}">` : `<div class="card-img-placeholder">📷</div>`}
+      ${p.foto
+        ? `<img class="card-img" src="${p.foto}" alt="${p.nombre}">`
+        : `<div class="card-img-placeholder">📷</div>`}
       <div class="card-body">
         <div class="card-name">${p.nombre}</div>
         <div class="card-price">${p.precio ? '$ ' + p.precio : 'Consultar precio'}</div>
